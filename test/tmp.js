e5abca2ad1b093bde9dee2c476126dd613c49ca2
Todd Wolfson <todd@twolfson.com>
2014-10-28 04:34:40 -0700

    Added proxies for all flavors

--- test/tmp.js
+++ test/tmp.js
@@ -4,7 +4,7 @@ var express = require('express');
 
 express().use(function beforeHandler (req, res, next) {
   // Record the request
-  console.log(req.method, req.url);
+  console.log('[icomoon.io]', req.method, req.url);
 
   // Add a randomizer to guarantee cache miss
   // DEV: This makes sure it isn't eight-track getting in the way
@@ -21,3 +21,43 @@ express().use(function beforeHandler (req, res, next) {
   //   return req;
   // }
 })).listen(1337);
+
+express().use(function beforeHandler (req, res, next) {
+  // Record the request
+  console.log('[i.icomoon.io]', req.method, req.url);
+
+  // Add a randomizer to guarantee cache miss
+  // DEV: This makes sure it isn't eight-track getting in the way
+  req.headers.hai = Math.random();
+
+  // Continue
+  next();
+}).use(eightTrack({
+  url: 'https://i.icomoon.io',
+  fixtureDir: __dirname + '/test_files/i-icomoon-http',
+  // normalizeFn: function (req) {
+  //   // Normalize the headers
+  //   // delete req.headers;
+  //   return req;
+  // }
+})).listen(1338);
+
+express().use(function beforeHandler (req, res, next) {
+  // Record the request
+  console.log('[perxis.com]', req.method, req.url);
+
+  // Add a randomizer to guarantee cache miss
+  // DEV: This makes sure it isn't eight-track getting in the way
+  req.headers.hai = Math.random();
+
+  // Continue
+  next();
+}).use(eightTrack({
+  url: 'http://perxis.com',
+  fixtureDir: __dirname + '/test_files/perxis-http',
+  // normalizeFn: function (req) {
+  //   // Normalize the headers
+  //   // delete req.headers;
+  //   return req;
+  // }
+})).listen(1339);
