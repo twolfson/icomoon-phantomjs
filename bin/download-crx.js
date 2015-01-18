9d3d38e1c91804873c8ab7b7a75691e5ed53bc9c
Todd Wolfson <todd@twolfson.com>
2015-01-17 19:15:52 -0600

    Fixed up code and everything magically works

--- bin/download-crx.js
+++ bin/download-crx.js
@@ -5,8 +5,8 @@ var request = require('request');
 
 // DEV: We wanted to use https://github.com/jiripospisil/chrome-ext-downloader for this but it seems broken
 // Generated via http://chrome-extension-downloader.com/how-does-it-work.php
-// var icomoonUrl = 'https://clients2.google.com/service/update2/crx?response=redirect&prodversion=38.0&x=id%3Dkppingdhhalimbaehfmhldppemnmlcjd%26installsource%3Dondemand%26uc';
-var icomoonUrl = 'http://google.com/';
+var icomoonUrl = 'https://clients2.google.com/service/update2/crx?response=redirect&prodversion=38.0&x=id%3Dkppingdhhalimbaehfmhldppemnmlcjd%26installsource%3Dondemand%26uc';
+// var icomoonUrl = 'http://google.com/';
 
 // Notify user we are downloading files
 console.log('Downloading "' + icomoonUrl + '"');
@@ -21,9 +21,14 @@ var req = request({url: icomoonUrl, encoding: null}, function handleContent (err
   // Notify user of completed download
   console.log('Download complete!');
 
+  // Cut off unused buffer section
+  var publicKeyLength = buff.readUInt32LE(8);
+  var signatureLength = buff.readUInt32LE(12);
+  var zipBuff = buff.slice(16 + publicKeyLength + signatureLength);
+
   // TODO: Clean up old cache files
 
   // Otherwise, extract zip content to disk
-  var zip = new AdmZip(buff);
+  var zip = new AdmZip(zipBuff);
   zip.extractAllTo(__dirname + '/.cache/');
 });
