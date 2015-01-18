73febdb34882ef5839e250724b49fa62bcbdf036
Todd Wolfson <todd@twolfson.com>
2015-01-17 19:19:32 -0600

    Completed install script for icomoon

--- bin/download-crx.js
+++ bin/download-crx.js
@@ -1,11 +1,18 @@
 // Load in our dependencies
+var fs = require('fs');
 var AdmZip = require('adm-zip');
-var progress = require('progress-stream');
 var request = require('request');
 
 // DEV: We wanted to use https://github.com/jiripospisil/chrome-ext-downloader for this but it seems broken
 // URL generated via http://chrome-extension-downloader.com/how-does-it-work.php
 var icomoonUrl = 'https://clients2.google.com/service/update2/crx?response=redirect&prodversion=38.0&x=id%3Dkppingdhhalimbaehfmhldppemnmlcjd%26installsource%3Dondemand%26uc';
+
+// If there is an old cache file, do nothing
+var targetDir = __dirname + '/../.cache/';
+if (fs.existsSync(targetDir)) {
+  console.log('`icomoon` already exists locally. Skipping download.');
+  process.exit();
+}
 
 // Notify user we are downloading files
 console.log('Downloading "' + icomoonUrl + '"');
@@ -25,9 +32,7 @@ var req = request({url: icomoonUrl, encoding: null}, function handleContent (err
   var signatureLength = buff.readUInt32LE(12);
   var zipBuff = buff.slice(16 + publicKeyLength + signatureLength);
 
-  // TODO: Clean up old cache files
-
   // Otherwise, extract zip content to disk
   var zip = new AdmZip(zipBuff);
-  zip.extractAllTo(__dirname + '/.cache/');
+  zip.extractAllTo(targetDir);
 });
