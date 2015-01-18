f60351b77c87af06b734f520de65f891a494bed7
Todd Wolfson <todd@twolfson.com>
2015-01-17 19:05:32 -0600

    Adding download script

---
+++ bin/download-crx.js
@@ -0,0 +1,23 @@
+// Load in our dependencies
+var AdmZip = require('adm-zip');
+var progress = require('progress');
+var request = require('request');
+
+// DEV: We wanted to use https://github.com/jiripospisil/chrome-ext-downloader for this but it seems broken
+// Generated via http://chrome-extension-downloader.com/how-does-it-work.php
+// var icomoonUrl = 'https://clients2.google.com/service/update2/crx?response=redirect&prodversion=38.0&x=id%3Dkppingdhhalimbaehfmhldppemnmlcjd%26installsource%3Dondemand%26uc';
+var icomoonUrl = 'http://google.com/';
+
+// Request our file
+var req = request(icomoonUrl, function handleContent (err, res, buff) {
+  // If there was an error, throw it
+  if (err) {
+    throw err;
+  }
+
+  // TODO: Clean up old cache files
+
+  // Otherwise, extract zip content to disk
+  var zip = new AdmZip(buff);
+  zip.extractAllTo(__dirname + '/.cache/');
+});
